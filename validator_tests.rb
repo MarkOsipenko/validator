75d77d22155a639a7d49a6ca10eb42b04f26a74b
Mark Osipenko <mark.osipenko@gmail.com>
2020-06-01 01:59:18 +0300

    prepare tests

--- validator_tests.rb
+++ validator_tests.rb
@@ -2,6 +2,20 @@
 
 require './user.rb'
 
-user1 = User.new(first_name: "Jon", last_name: 'Travolta', age: 50)
+class ValidatorTest
+  def test(user = User.new, result)
+    if user.valid? == result
+      "\e[32m#{'+'}\e[0m"
+    else
+      "\e[31m#{'F'}\e[0m"
+    end
+  end
+end
 
-p "#{user1.first_name} #{user1.last_name}, #{user1.age} years, valid: #{user1.valid?}"
+valid_user1 =   User.new(first_name: "Jon", last_name: 'Travolta', age: 50)
+invalid_user1 = User.new(first_name: "", last_name: 'Travolta', age: 50)
+
+puts [
+  ValidatorTest.new.test(valid_user1, true),
+  ValidatorTest.new.test(invalid_user1, true)
+].join(" ")
